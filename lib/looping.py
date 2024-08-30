e81e01f7291469e4bc7fe84625699cd0e1bdbfd1
kenedy4 <kennedymusyoki8@gmail.com>
2024-08-30 14:44:34 +0300

    While loop for counter done

--- lib/looping.py
+++ lib/looping.py
@@ -2,11 +2,20 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    counters = 0
+    while counters < 10:
+            counters += 1
+            print(counters)
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+   square_integers = []
+   for num in int_list:
+        if num % 2 == 0 and num % 3 == 0:
+            square_integers.append(num**2)
+    return square_integers
+    
 
 def fizzbuzz():
     # code goes here!
