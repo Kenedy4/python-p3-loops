6e5f9f32aaefa4268fcacd25c285a3c48cc7ee6d
kenedy4 <kennedymusyoki8@gmail.com>
2024-08-30 14:52:15 +0300

    Square error done

--- lib/looping.py
+++ lib/looping.py
@@ -10,13 +10,18 @@ def happy_new_year():
 
 def square_integers(int_list):
     # code goes here!
-   square_integers = []
-   for num in int_list:
-        if num % 2 == 0 and num % 3 == 0:
-            square_integers.append(num**2)
-    return square_integers
+    return [i ** 2 for i in int_list]
+
     
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
